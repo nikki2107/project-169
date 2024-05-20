cc9e914a9ee36d338a3a3121fde166c72f9e7f38
nikki2107 <71547789+nikki2107@users.noreply.github.com>
2024-05-20 09:49:48 +0530

    Add files via upload

---
+++ js/markerhandler.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent("markerhandler", {
+  init: async function() {
+
+  },
+  handleMarkerFound: function() {
+    // Changing button div visibility
+    
+//call the required buttons 
+
+    // Handling Click Events
+ 
+
+  
+  },
+
+  handleMarkerLost: function() {
+    // Changing button div visibility
+
+  }
+});
